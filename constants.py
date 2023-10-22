20a6940c18af3655eb5e87b09da529ebf3d451c0
fjovanovic <filipjovanovic98@gmail.com>
2023-10-22 13:59:48 +0200

    Added url for OHLC data

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ COINGECKO_COINS_PER_PAGE = 'https://api.coingecko.com/api/v3/coins/markets?vs_cu
 COINGECKO_COIN_DATA_URL = 'https://api.coingecko.com/api/v3/coins/COIN_ID_REPLACE?tickers=false&market_data=true&community_data=false&developer_data=false'
 COINGECKO_ALL_COINS_URL = 'https://api.coingecko.com/api/v3/coins/list?include_platform=false' 
 COINGECKO_PRICE_CHANGE_LAST_HOUR = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h'
-BINANCE_PRICE_ALL_COINS = 'https://api.binance.com/api/v3/ticker/price?'
+COINGECKO_OHLC_DATA_URL = 'https://api.coingecko.com/api/v3/coins/COIN_ID_REPLACE/ohlc?vs_currency=usd&days=1&precision=1'
 
 NEW_ATH_IMAGE = 'https://cdn.iconscout.com/icon/premium/png-256-thumb/upward-arrow-2206687-1853775.png'
 LAST_HOUR_MOVERS_IMAGE = 'https://img.favpng.com/6/10/11/stock-market-bull-priceu2013earnings-ratio-wish-png-favpng-gXsPEtZiBZEULjz07u2j4k5v7.jpg'
