36ed358428fd746760d1ebb0bacf0b802ebd3485
fjovanovic <filipjovanovic98@gmail.com>
2024-03-31 01:26:57 +0100

    Refactoring

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ from components import MyBot, MyCommandTree
 from utils import utils
 
 
-load_dotenv()
-
-bot = MyBot(command_prefix='$', activity_name='Blockchain', tree_cls=MyCommandTree)
-
-
 async def main():
-    parser = ArgumentParser()
+    load_dotenv()
+    
+    bot = MyBot(command_prefix='$', activity_name='Blockchain', tree_cls=MyCommandTree)
+
+    parser = ArgumentParser(
+        usage='python3 main.py [-t | --test]',
+        description='Discord bot for providing information about the cryptocurrencies',
+        allow_abbrev=False
+    )
+
     parser.add_argument('-t', '--test', action='store_true', help='Providing console log instead of file log')
-    args = parser.parse_args()
+    args = parser.parse_args()    
     if args.test:
         discord.utils.setup_logging()
     else:
-        utils.setup_logging()
+        utils.setup_file_logging()
     
     TOKEN = os.getenv('TOKEN')
     await bot.start(TOKEN)
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
